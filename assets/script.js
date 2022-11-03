f38ac3df3b372dbf3c5aea91b03446a50d4c9a6a
Marc Norris <marcnorris007@icloud.com>
2022-11-03 09:51:04 +0000

    We have added JavaScript to our site and leanred about arrays and for loops

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
+
+
+
+
+/*
+var names = ["Marc", "Becky", "Willow", "Elsie"];
+
+for(var x = 0; x < names.length; x++) {
+    console.log(names[x]);
+}
+
+var cars = ["astra", "golf", "mini"];
+for(var x = 0; x <cars.length; x++) {
+    console.log(cars[x]);
+}
+*/
